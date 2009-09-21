8dea1a2abbeae55aceb372441305c674a372b54d
Bruce Kroeze <bkroeze@gmail.com>
2009-09-20 18:18:26 -0700

    adding test for autosuccess

--- bursar/signals.py
+++ bursar/signals.py
@@ -1,6 +1,5 @@
 import django.dispatch
 
 confirm_sanity_check = django.dispatch.Signal()
-payment_choices = django.dispatch.Signal()
 payment_complete = django.dispatch.Signal()
 payment_methods_query = django.dispatch.Signal()
