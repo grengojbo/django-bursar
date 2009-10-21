1e35e8aac5f3b13afe8c27b4954360042039690a
Bruce Kroeze <bkroeze@gmail.com>
2009-10-21 11:57:26 -0700

    preparing the remaining gateways

--- bursar/gateway/trustcommerce_gateway/views.py
+++
@@ -1,8 +0,0 @@
-from livesettings import config_get_group
-from payment.views import confirm, payship
-    
-def pay_ship_info(request):
-    return payship.credit_pay_ship_info(request, config_get_group('PAYMENT_TRUSTCOMMERCE'))
-    
-def confirm_info(request):
-    return confirm.payment_confirm_info(request, config_get_group('PAYMENT_TRUSTCOMMERCE'))
